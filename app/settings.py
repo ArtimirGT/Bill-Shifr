5d7199657509ea7844749d6c1ca2a4402523bf94
avgustOFF <dmitrijavgustov7@gmail.com>
2024-04-16 19:52:29 +0300

    add butons and txt

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,24 @@
-win_x, win_y = 500, 500
-Title = 'BIll Shifr'
-x, y = 200, 200
+win_width, win_height = 800, 800
+win_x, win_y = 200, 200
+txt_title = 'BIll Shifr'
+txt_manual = 'Lorem ipsum dolor sit amet, consectetur \n\
+adipiscing elit, sed do eiusmod \n\
+tempor incididunt ut labore et dolore magna aliqua.\n\
+ Ut enim ad minim veniam, \n\
+quis nostrud exercitation ullamco laboris \n\
+nisi ut aliquip ex ea commodo consequat. \n\
+Duis aute irure dolor in reprehenderit in voluptate \n\
+velit esse cillum dolore eu fugiat nulla pariatur. \n\
+Excepteur sint occaecat cupidatat non proident, \n\
+sunt in culpa qui officia deserunt mollit anim id \n\
+est laborum.'
+txt_shif_1 = 'шифр1'
+txt_shif_2 = 'шифр2'
+txt_shif_3 = 'шифр3'
+txt_shif_4 = 'шифр4'
+txt_shif_5 = 'шифр5'
+txt_shif_6 = 'шифр6'
+txt_shif_7 = 'шифр7'
+txt_shif_8 = 'шифр8'
+txt_shif_9 = 'шифр9'
+txt_shif_10 = 'шифр10'
